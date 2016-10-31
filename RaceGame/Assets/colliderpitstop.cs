12394b19e3adf0f90e357d45b195bc9d008cae2b
Vene 1602 <vene1602@student.nhl.nl>
2016-10-31 14:33:24 +0100

    versie 0.2

---
+++ RaceGame/Assets/colliderpitstop.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colliderpitstop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "PickUp")
+        {
+            Debug.Log("Tanken");
+            Debug.Log("Tanken");
+            Debug.Log("Tanken");
+            Debug.Log("Tanken");
+            Debug.Log("Tanken");
+        }
+    }
+
+
+}
