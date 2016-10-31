a003eb030030b127acab55301057cef43e2bc14c
Jildert Venema <Jildert Venema>
2016-10-31 20:54:35 +0100

    versie 0.3

---
+++ RaceGame/Assets/fuelmeter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class fuelmeter : MonoBehaviour {
+
+    public Autocontrol objectToMeasure;
+    public float maxgass = 1000.0f;
+
+    private Image image;
+
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        image.fillAmount = objectToMeasure.gas/maxgass;
+	}
+}
