d7504d904fd2ccc213a09ad924010101bd853954
Vene 1602 <vene1602@student.nhl.nl>
2016-11-01 15:44:07 +0100

    versie 0.4

---
+++ RaceGame/Assets/winscript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class winscript : MonoBehaviour {
+
+
+    public Autocontrol auto;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (auto.laps == 3)
+        {
+            Text wintext = GetComponent<Text>();
+            wintext.text = ("Player: " + auto.playernumber + " wins!");
+        }
+    }
+}
